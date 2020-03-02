2e370251478bf10a1d2cb355376bd16406d3e0be
wggreen <wggreen.nss@gmail.com>
2020-03-02 10:18:20 -0600

    final push

---
+++ exercises/dictionaries/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+            // Add Dictionary to your `dictionaryOfWords` list
+
+            dictionaryOfWords.Add(excitedWord);
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> dictionaryWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            dictionaryWord.Add("word", "dictionary");
+            dictionaryWord.Add("definition", "a book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.");
+            dictionaryWord.Add("part of speech", "noun");
+            dictionaryWord.Add("example sentence", "I need to learn more words so I'm going to read the dictionary.");
+
+            dictionaryOfWords.Add(dictionaryWord);
+
+
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach (Dictionary<string, string> dictionary in dictionaryOfWords)
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> word in dictionary)
+                {
+                    Console.WriteLine($"{word.Key}: {word.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ exercises/dictionaries2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
+            idioms.Add("Penny", new List<string> { "A", "penny", "for", "your", "thoughts" });
+            idioms.Add("Injury", new List<string> { "Add", "insult", "to", "injury" });
+            idioms.Add("Moon", new List<string> { "Once", "in", "a", "blue", "moon" });
+            idioms.Add("Grape", new List<string> { "I", "heard", "it", "through", "the", "grapevine" });
+            idioms.Add("Murder", new List<string> { "Kill", "two", "birds", "with", "one", "stone" });
+            idioms.Add("Limbs", new List<string> { "It", "costs", "an", "arm", "and", "a", "leg" });
+            idioms.Add("Grain", new List<string> { "Take", "what", "someone", "says", "with", "a", "grain", "of", "salt" });
+            idioms.Add("Fences", new List<string> { "I'm", "on", "the", "fence", "about", "it" });
+            idioms.Add("Sheep", new List<string> { "Pulled", "the", "wool", "over", "his", "eyes" });
+            idioms.Add("Lucifer", new List<string> { "Speak", "of", "the", "devil" });
+
+            foreach (KeyValuePair<string, List<string>> word in idioms)
+            {
+                Console.WriteLine($" {word.Key}: {String.Join(" ", word.Value)}. ");
+
+            };
+        }
+    }
+
+}
